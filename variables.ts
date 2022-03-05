7b177c967814930628cf92ed16a7655df770a8e1
Tolan Beniamin <39988307+xTbeniaminTx@users.noreply.github.com>
2022-03-05 10:56:15 +0100

    interfaces in ts

--- variables.ts
+++ variables.ts
@@ -1,3 +1,13 @@
-//strig, number, boolean, null, undefined
-let myName:string = "Benjamin";
-myName = 78;
+interface IAccount {
+    name: string,
+        balance: number,
+        status ? : string,
+        deposit ? : () => void
+};
+
+const account: IAccount = {
+    name: "Louis",
+    balance: 55
+};
+
+let accounts: IAccount[];
